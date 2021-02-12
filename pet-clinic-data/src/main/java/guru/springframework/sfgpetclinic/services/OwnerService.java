c5ebaa21846f467bba875979f384ed33c307ab52
mario.magno <mgmagnoj@gmail.com>
2021-02-12 15:07:16 -0300

    Ok

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/OwnerService.java
@@ -2,7 +2,7 @@ package guru.springframework.sfgpetclinic.services;
 
 import guru.springframework.sfgpetclinic.model.Owner;
 
-public interface OwnerService extends CrudService {
+public interface OwnerService extends CrudService<Owner, Long> {
     
     Owner findByLastName(String lastName);    
     

6316f79b718fc28c52ce649f3ed43b5b172db748
mario.magno <mgmagnoj@gmail.com>
2021-02-16 16:15:49 -0300

    Ok 122

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
@@ -1,5 +1,48 @@
 package guru.springframework.sfgpetclinic.model;
 
-public class Owner extends Person{
+import java.util.Set;
+
+public class Owner extends Person {
+
+    private String address;
+    private String city;
+    private String fone;
+    private Set<Pet> pets;
+
+
+    public String getAddress() {
+        return this.address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return this.city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getFone() {
+        return this.fone;
+    }
+
+    public void setFone(String fone) {
+        this.fone = fone;
+    }
+
+
+    public Set<Pet> getPets() {
+        return this.pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
+
+    
     
 }
--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -5,30 +5,48 @@ import org.springframework.stereotype.Component;
 
 import guru.springframework.sfgpetclinic.model.Owner;
 import guru.springframework.sfgpetclinic.model.Pet;
+import guru.springframework.sfgpetclinic.model.PetType;
 import guru.springframework.sfgpetclinic.model.Vet;
 import guru.springframework.sfgpetclinic.services.OwnerService;
+import guru.springframework.sfgpetclinic.services.PetService;
+import guru.springframework.sfgpetclinic.services.PetTypeService;
 import guru.springframework.sfgpetclinic.services.VetService;
-
 
 @Component
 public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
 
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
 
-        Long i = (long) 0;
+        Long i = (long) 1;
+
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType saveDogPetType = petTypeService.save(dog);
+
+        System.out.println("Save Pet Type DOG: " + i++);
+
+        PetType cat = new PetType();
+        dog.setName("Cat");
+        PetType saveCatPetType = petTypeService.save(cat);
+
+        System.out.println("Save Pet Type CAT: " + i++);
+
+        i = (long) 0;
 
         Owner owner1 = new Owner();
-        //owner1.setId(i++);
+        owner1.setId(++i);
         owner1.setFirstName("Michael");
         owner1.setLastName("Weston");
 
@@ -38,10 +56,10 @@ public class DataLoader implements CommandLineRunner {
 
         ownerService.save(owner1);
 
-        System.out.println("Loaded Owner: "+ i);
-        
+        System.out.println("Loaded Owner: " + i);
+
         Owner owner2 = new Owner();
-        //owner2.setId(i++);
+        owner2.setId(++i);
         owner2.setFirstName("Fiona");
         owner2.setLastName("Glenanne");
 
@@ -49,24 +67,28 @@ public class DataLoader implements CommandLineRunner {
         fionasCat.setOwner(owner2);
 
         ownerService.save(owner2);
+
+        System.out.println("Loaded Owner: " + i);
+
+        i = (long) 0;
         
-        System.out.println("Loaded Owner: "+ i);
-    
         Vet vet1 = new Vet();
-        //vet1.setId(i++);
+        vet1.setId(++i);
         vet1.setFirstName("Sam");
         vet1.setLastName("Axe");
 
         vetService.save(vet1);
 
-        System.out.println("Loaded Vets....");
         
+
+        System.out.println("Loaded Vets...." + i);
+
         Vet vet2 = new Vet();
-        //vet2.setId(i++);
+        vet2.setId(++i);
         vet2.setFirstName("Jessie");
         vet2.setLastName("Porter");
 
-        System.out.println("Loaded Vets....");
+        System.out.println("Loaded Vets...."+ i);
         vetService.save(vet2);
     }
 

5224db6ee8285d7699c2bdeee02dc4cb56e4ac96
mario.magno <mgmagnoj@gmail.com>
2021-02-18 14:42:48 -0300

    Closes #20

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/VetServiceMap.java
@@ -4,34 +4,52 @@ import java.util.Set;
 
 import org.springframework.stereotype.Service;
 
+import guru.springframework.sfgpetclinic.model.Specialty;
 import guru.springframework.sfgpetclinic.model.Vet;
+import guru.springframework.sfgpetclinic.services.SpecialtiesService;
 import guru.springframework.sfgpetclinic.services.VetService;
 
 @Service
 public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
-    
+
+    private SpecialtiesService specialtiesService;
+
+    public VetServiceMap(SpecialtiesService specialtiesService) {
+        this.specialtiesService = specialtiesService;
+    }
+
     @Override
-    public Set<Vet> findAll(){
+    public Set<Vet> findAll() {
         return super.findAll();
     }
 
     @Override
-    public Vet findById(Long id){
+    public Vet findById(Long id) {
         return super.findById(id);
     }
+
     @Override
     public Vet save(Vet object){
+
+        if (object.getSpecialties().isEmpty()){
+                object.getSpecialties().forEach(specialty -> {
+                    if (specialty.getId() ==null) {
+                        Specialty savedSpecialty = specialtiesService.save(specialty);
+                        specialty.setId(savedSpecialty.getId());
+                    }
+                });
+        }
         return super.save(object);
     }
+    
 
-    @Override 
-    public void delete(Vet object){
+    @Override
+    public void delete(Vet object) {
         super.delete(object);
     }
 
     @Override
-    public void deleteById(Long id){
+    public void deleteById(Long id) {
         super.deleteById(id);
     }
 }
-    
